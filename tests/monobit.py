80e1e4d4472fd2fd91ad3ddb77daf59226472e2d
takuya <tkykwsm6ch@gmail.com>
2020-02-20 14:50:55 +0900

    [add] monobit test

---
+++ tests/monobit.py
@@ -0,0 +1,33 @@
+# monobit.py
+
+
+import math
+import numpy as np
+
+
+def monobit(bits):
+    """ Frequency (monobit) Test
+
+    Parameters
+    ----------
+    bits (ndarray uint8) :
+        Binary sequence to be tested.
+    
+    Returns
+    -------
+    p_value (float) : Test result.
+
+    ones (int) : Number of 1s in the sequence.
+
+    zeros (int) : Number of 0s in the sequence.
+    """
+
+    ones = np.count_nonzero(bits)
+    zeros = bits.size - ones
+
+    s_obs = abs(ones-zeros) / math.sqrt(bits.size)
+    f = s_obs / math.sqrt(2.0)
+
+    p_value = math.erfc(f)
+
+    return p_value, ones, zeros
