b734d762d79b2e50b2efd18013669d7798e3f613
takuya <tkykwsm6ch@gmail.com>
2020-02-21 11:50:31 +0900

    [update] Replac for loop with numpy function.

--- tests/frequency.py
+++ tests/frequency.py
@@ -2,32 +2,40 @@
 
 import numpy as np
 from scipy.special import gammaincc
-from fractions import Fraction
 
 
-def frequency(bits, block_size):
+def frequency(bits, block_size=128):
     """ Frequency Test within a block
 
     Parameters
     ----------
-    bits (ndarray, uint8, 1d) : Binary sequence to be tested.
-    block_size (int) : Sequence length after being divided into blocks.
+    bits : 1d-ndarray uint8
+        Binary sequence to be tested.
+    block_size : int
+        Sequence length after being divided into blocks.
     
     Returns
     -------
-    p_value (float) : Test result.
-    chi_squared (float) : Computed statistics.
+    p_value : float
+        Test result.
+    chi_squared : float
+        Computed statistics.
     """
     block_num, mod = divmod(bits.size, block_size)  # Number of blocks
     
-    s = Fraction(0,1)
-    for i in range(block_num):
-        block_sum = np.count_nonzero(bits[i*block_size : (i+1)*block_size])
-        pi = Fraction(block_sum, block_size)
-        v = pi - Fraction(1,2)
-        s += v**2
+    blocks = np.resize(bits, (block_num, block_size))
+    block_sums = np.sum(blocks, axis=1)
+    v = block_sums/block_size - 0.5
+    s = np.sum(np.power(v,2))
     chi_squared = 4.0 * block_size * s
 
     p_value = gammaincc(block_num/2.0 , chi_squared/2.0)
         
-    return (p_value, chi_squared)
+    return p_value, chi_squared
+
+
+if __name__ == "__main__":
+
+    bits = np.random.randint(0, 2, size=1000)
+    results = frequency(bits)
+    print("\np-value = {},  chi_squared = {}\n".format(results[0], results[1]))
