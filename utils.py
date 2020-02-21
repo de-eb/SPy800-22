de396d0b59d93bb7fdbd734de9d5063bd9680787
takuya <tkykwsm6ch@gmail.com>
2020-02-21 10:56:34 +0900

    [update] constructor

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import csv
 import time
 import psutil
 import numpy as np
+from importlib import import_module
 from multiprocessing import Pool
 
 
@@ -59,37 +60,81 @@ class BitShortageError(STSError):
 
 class STS:
     """
+    NIST SP800-22 rev.1a (STS2.1.2) manager class
+    
+    Parameters
+    ----------
+    path : str
+        Path of the file to be tested.
+    fmt : int
+        Specifies how each byte in the file is converted to bits. If 0, it
+        converts 0x30 and 0x31 to 0 and 1 (that is, ASCII characters 0 or 1).
+        If 1, convert 0x00 and 0x01 to 0 and 1, respectively. If 2 or 3, each
+        bit is used as it is. If 2, each byte is read from LSB (little
+        endian). If 3, each byte is read from MSB (big endian).
+    seq_num : int
+        Number of sequences to be tested individually.
+    seq_size : int
+        Bit length of each sequence.
+    proc_num : int
+        Number of processes for running tests in parallel.
+    choice : tuple of int
+        A tuple of IDs of the tests to run. If None, run all tests.(0-monobit,
+        1-block_frequency, 2-runs, 3-longest_run, 4-rank, 5-dft,
+        6-non_overlapping, 7-overlapping, 8-universal, 9-complexity, 10-serial,
+        11-entropy, 12-cusums, 13-excursions, 14-excursions_variant)
+    bs_freq : int
+        Block size in "block frequency" test.
+    bs_notm : int
+        Block size in "non-overlapping template matching" test.
+    bs_otm : int
+        Block size in "overlapping template matching" test.
+    bs_comp : int
+        Block size in "liner complexity" test.
+    bs_ser : int
+        Block size in "serial" test.
+    bs_apen : int
+        Block size in "approximate entropy" test.
     """
+    # Test sources
+    SRC = [ 'monobit', 'frequency', 'runs', 'longest_run', 'rank', 'dft',
+            'non_overlapping', 'overlapping', 'universal', 'complexity',
+            'serial', 'entropy', 'cusum', 'excursions', 'excursions_var']
     NAMES = [  # Test Specifiers
-        {'file' : "monobit",         'title' : "Frequency (monobit)"},
-        {'file' : "frequency",       'title' : "Frequency within a block"},
-        {'file' : "runs",            'title' : "Runs"},
-        {'file' : "longest_run",     'title' : "Longest run of ones in a block"},
-        {'file' : "rank",            'title' : "Binary matrix rank"},
-        {'file' : "dft",             'title' : "Discrete fourier transform (spectral)"},
-        {'file' : "non_overlapping", 'title' : "Non-overlapping template matching"},
-        {'file' : "overlapping",     'title' : "Overlapping template matching"},
-        {'file' : "universal",       'title' : "Maurer\'s \"universal statistical\""},
-        {'file' : "complexity",      'title' : "Linear complexity"},
-        {'file' : "serial",          'title' : "Serial"},
-        {'file' : "entropy",         'title' : "Approximate entropy"},
-        {'file' : "cusum",           'title' : "Cumulative sums (Cusum)"},
-        {'file' : "excursions",      'title' : "Random excursions"},
-        {'file' : "excursions_var",  'title' : "Random excursions variant"}
+        {'func' : "monobit",         'title' : "Frequency (monobit)"},
+        {'func' : "frequency",       'title' : "Frequency within a block"},
+        {'func' : "runs",            'title' : "Runs"},
+        {'func' : "longest_run",     'title' : "Longest run of ones in a block"},
+        {'func' : "rank",            'title' : "Binary matrix rank"},
+        {'func' : "dft",             'title' : "Discrete fourier transform (spectral)"},
+        {'func' : "non_overlapping", 'title' : "Non-overlapping template matching"},
+        {'func' : "overlapping",     'title' : "Overlapping template matching"},
+        {'func' : "universal",       'title' : "Maurer\'s \"universal statistical\""},
+        {'func' : "complexity",      'title' : "Linear complexity"},
+        {'func' : "serial",          'title' : "Serial"},
+        {'func' : "entropy",         'title' : "Approximate entropy"},
+        {'func' : "cusum",           'title' : "Cumulative sums (Cusum)"},
+        {'func' : "excursions",      'title' : "Random excursions"},
+        {'func' : "excursions_var",  'title' : "Random excursions variant"}
     ]
-    FORMATS = ['ASCII_01', 'Byte_01', 'Little-endian', 'Big-endian']  # input modes
+    # Input file formats
+    FORMATS = ['ASCII_01', 'Byte_01', 'Little-endian', 'Big-endian']
 
     
-    def __init__(self, path, fmt, seq_num, seq_size, proc_num=1, choice=[0],
-                    bs_freq=128, bs_notm=9, bs_otm=9, bs_comp=500, bs_ser=16, bs_apen=10):
+    def __init__(self, path, fmt, seq_num, seq_size, proc_num=1, choice=None,
+        bs_freq=128, bs_notm=9, bs_otm=9, bs_comp=500, bs_ser=16, bs_apen=10):
         """Constructor
         """
         if not os.path.isfile(path):
             msg = "File \"{}\" is not found.".format(path)
             raise InvalidSettingError(msg)
-        if fmt not in [0, 1, 2, 3]:
-            msg = "File format must be specified as either 0, 1, 2, or 3."
+        self.file_path = path
+
+        if fmt not in range(len(STS.FORMATS)):
+            msg = "File format must be between 0 and {}.".format(len(STS.FORMATS))
             raise InvalidSettingError(msg)
+        self.file_format = fmt
+
         if seq_num < 1:
             msg = "Number of sequences must be 1 or more."
             raise InvalidSettingError(msg)
@@ -99,31 +144,32 @@ class STS:
         if seq_num*seq_size >= psutil.virtual_memory().available*0.8:
             msg = "Memory may be insufficient. Reduce the number or length of the sequence."
             raise InvalidSettingError(msg)
+        self.sequence_num = seq_num
+        self.sequence_size = seq_size
+
         if not (0 < proc_num <= os.cpu_count()):
             msg = "Number of processes must be between 1 and {}".format(os.cpu_count())
             raise InvalidSettingError(msg)
-        if not set(choice) <= set([i for i in range(16)]):
-            msg = "Test choice must be specified as a list from 0 to 15."
+        self.process_num = proc_num
+
+        if choice is None:
+            self.test_choice = {i for i in range(len(STS.NAMES))}
+        elif type(choice) == 'tuple':
+            if not set(choice) <= {i for i in range(len(STS.NAMES))}:
+                msg = "Test choice must be between 0 and 14."
+                raise InvalidSettingError(msg)
+            else:
+                self.test_choice = set(choice)
+        else:
+            msg = "Test choice must be specified as None or tuple."
             raise InvalidSettingError(msg)
+
         if min([bs_freq, bs_notm, bs_otm, bs_comp, bs_ser, bs_apen]) < 1:
             msg = "Block sizes must be 1 or more."
             raise InvalidSettingError(msg)
-        self.file_path = path
-        self.file_format = fmt
-        self.sequence_num = seq_num
-        self.sequence_size = seq_size
-        self.process_num = proc_num
-        self.block_size_freq = bs_freq
-        self.block_size_notm = bs_notm
-        self.block_size_otm = bs_otm
-        self.block_size_comp = bs_comp
-        self.block_size_ser = bs_ser
-        self.block_size_apen = bs_apen
-        if 0 in set(choice):
-            self.test_choice = [i+1 for i in range(15)]
-        else:
-            self.test_choice = list(set(choice))
-
+        self.block_sizes = {1 : bs_freq, 6 : bs_notm, 7 : bs_otm,
+                            9 : bs_comp, 10 : bs_ser, 11 : bs_apen}
+    
     def show_settings(self):
         """
         """
@@ -138,14 +184,14 @@ class STS:
         print("      Number of proccesses : {}".format(self.process_num))
         print("\n    List of tests to be run\n")
         for i in self.test_choice:
-            print("      {:<2}: {}".format(i, self.NAMES[i-1]['title']))
+            print("      {:<2}: {}".format(i, STS.NAMES[i-1]['title']))
         print("\n    Block sizes\n")
-        print("      {:<34}: {}".format(self.NAMES[1]['title'], self.block_size_freq))
-        print("      {:<34}: {}".format(self.NAMES[6]['title'], self.block_size_notm))
-        print("      {:<34}: {}".format(self.NAMES[7]['title'], self.block_size_otm))
-        print("      {:<34}: {}".format(self.NAMES[9]['title'], self.block_size_comp))
-        print("      {:<34}: {}".format(self.NAMES[10]['title'], self.block_size_ser))
-        print("      {:<34}: {}".format(self.NAMES[11]['title'], self.block_size_apen))
+        print("      {:<34}: {}".format(STS.NAMES[1]['title'], self.block_size_freq))
+        print("      {:<34}: {}".format(STS.NAMES[6]['title'], self.block_size_notm))
+        print("      {:<34}: {}".format(STS.NAMES[7]['title'], self.block_size_otm))
+        print("      {:<34}: {}".format(STS.NAMES[9]['title'], self.block_size_comp))
+        print("      {:<34}: {}".format(STS.NAMES[10]['title'], self.block_size_ser))
+        print("      {:<34}: {}".format(STS.NAMES[11]['title'], self.block_size_apen))
         print()
         print("**************************************************************************\n")
 
@@ -203,11 +249,14 @@ class STS:
                     n += 1
         raise BitShortageError(n+1, self.sequences.size)
 
-    def invoke_test(self, test, seq):
+    def invoke_test(self, test_id, seq_id):
         """
         """
-        message = " {}  for  {}\n".format(test_name, file_path)
-        test = getattr(__import__("sp800_22_" + test_name), test_name)
+        test = getattr(
+            import_module("tests." + STS.NAMES[test_id-1]['func']),
+            STS.NAMES[test_id]['func'])
+        if test_id -1 == 1:
+            results = test(self.sequences[seq_id], )
 
         try:
             success, p_val, p_val_list, msg = test(bits)
@@ -270,11 +319,14 @@ class STS:
 
 if __name__ == "__main__":
 
-    sts = STS(
-        path = ".debug/bits.txt",
-        fmt = 0,
-        seq_num = 10,
-        seq_size = 1000
-    )
+    # sts = STS(
+    #     path = ".debug/bits.txt",
+    #     fmt = 0,
+    #     seq_num = 10,
+    #     seq_size = 1000
+    # )
 
-    sts.read_bits()
+    # sts.read_bits()
+
+    a = None
+    set()
