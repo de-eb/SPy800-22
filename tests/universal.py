cecaa2daa31cb5efff77c37c2c95a4fd3eff46c8
takuya <tkykwsm6ch@gmail.com>
2020-02-25 02:08:55 +0900

    [add] maurers universal statistical test

---
+++ tests/universal.py
@@ -0,0 +1,65 @@
+# universal.py
+
+import math
+import numpy as np
+
+
+def packbits(x, reverse=True):
+    p = np.power(2, np.arange(x.shape[-1]))
+    if reverse:
+        p = p[::-1]
+    return np.dot(x, p)
+
+def universal(bits):
+    """
+    Maurer's "Universal Statistical" Test
+
+    Parameters
+    ----------
+    bits : 1d-ndarray uint8
+        Binary sequence to be tested.
+    
+    Returns
+    -------
+    p_value : float
+        Test result.
+    phi : float
+        Computed statistics.
+    """
+    blk_size = 5
+    thresh = [387840, 904960, 2068480, 4654080, 10342400, 22753280,
+                49643520, 107560960, 231669760, 496435200, 1059061760]
+    for i in thresh:
+        if bits.size >= i:
+            blk_size += 1
+    if blk_size < 6:
+        return 0.0, None
+    var = [0, 0, 0, 0, 0, 0, 2.954, 3.125, 3.238, 3.311,
+            3.356, 3.384, 3.401, 3.410, 3.416, 3.419, 3.421]
+    expected = [0, 0, 0, 0, 0, 0, 5.2177052, 6.1962507,
+                7.1836656, 8.1764248, 9.1723243, 10.170032,
+                11.168765, 12.168070, 13.167693, 14.167488, 15.167379]
+    q = 10 * 2**blk_size
+    k = bits.size // blk_size - q
+    sigma = ((0.7 - 0.8/blk_size + (4+32/blk_size)/15 * k**(-3/blk_size))
+            * math.sqrt(var[blk_size]/k))
+
+    t = np.zeros(2**blk_size)
+    blk = packbits(np.resize(bits, (k+q, blk_size)))
+    uniq, idx = np.unique(blk[:q][::-1], return_index=True)
+    t[uniq] = idx
+    s = 0
+    for i in range(t.size):
+        s += np.sum(np.log2(np.diff(np.append(-t[i], np.where(blk[q:]==i)))))
+    phi = s / k
+    
+    p_value = math.erfc(abs(phi-expected[blk_size]) / (math.sqrt(2)*sigma))
+
+    return p_value, phi
+
+
+if __name__ == "__main__":
+
+    bits = np.random.randint(0, 2, size=2**20)
+    results = universal(bits)
+    print("\np-value = {}\n".format(results[0]))
