cf4cee1e3b3019eaf0d65f32cc0f5ff33f34a6db
takuya <tkykwsm6ch@gmail.com>
2020-02-27 17:18:30 +0900

    [update] remove extra variables, fix comments.

--- tests/frequency.py
+++ tests/frequency.py
@@ -4,39 +4,37 @@ import numpy as np
 from scipy.special import gammaincc
 
 
-def frequency(bits, block_size=128):
+def frequency(bits, blk_size=128):
     """
-    Frequency Test within a block
+    Frequency Test within a Block.
+    Evaluate the uniformity of 0s and 1s for each M-bit subsequence.
 
     Parameters
     ----------
     bits : 1d-ndarray uint8
         Binary sequence to be tested.
-    block_size : int
-        Sequence length after being divided into blocks.
+    blk_size : int
+        Bit length of each block.
     
     Returns
     -------
     p_value : float
         Test result.
     chi_square : float
-        Computed statistics.
+        Computed statistic.
     """
-    block_num, mod = divmod(bits.size, block_size)  # Number of blocks
-    
-    blocks = np.resize(bits, (block_num, block_size))
-    block_sums = np.sum(blocks, axis=1)
-    v = block_sums / block_size - 0.5
-    s = np.sum(np.power(v,2))
-    chi_square = 4.0 * block_size * s
+    blk_num = bits.size // blk_size
+    blk = np.resize(bits, (blk_num, blk_size))
+    sigma = np.sum((np.sum(blk, axis=1)/blk_size - 0.5)**2)
+    chi_square = 4 * blk_size * sigma
 
-    p_value = gammaincc(block_num/2.0 , chi_square/2.0)
+    p_value = gammaincc(blk_num/2 , chi_square/2)
         
     return p_value, chi_square
 
 
 if __name__ == "__main__":
 
-    bits = np.random.randint(0, 2, size=1024)
+    bits = np.random.randint(0, 2, size=10**6)
     results = frequency(bits)
     print("\np-value = {}\n".format(results[0]))
