c77e6169917bc7ffa81c77da2590201b963a9fa9
takuya <tkykwsm6ch@gmail.com>
2020-03-03 15:15:39 +0900

    [update] Classification, added compatibility

--- tests/frequency.py
+++ tests/frequency.py
@@ -1,41 +1,79 @@
 # frequency.py
 
+from tests.base import Base, InvalidSettingError
 import numpy as np
 from scipy.special import gammaincc
 
 
-def frequency(bits, blk_size=128):
+class BlockFrequencyTest(Base):
     """
     Frequency Test within a Block
-    -----------------------------
-    Evaluate the uniformity of 0s and 1s for each M-bit subsequence.
+    =============================
 
-    Parameters
+    Attributes
     ----------
-    bits : 1d-ndarray uint8
-        Binary sequence to be tested.
-    blk_size : int
-        Bit length of each block.
-    
-    Returns
-    -------
-    p_value : float
-        Test result.
-    chi_square : float
-        Computed statistic.
     """
-    blk_num = bits.size // blk_size
-    blk = np.resize(bits, (blk_num, blk_size))
-    sigma = np.sum((np.sum(blk, axis=1)/blk_size - 0.5)**2)
-    chi_square = 4 * blk_size * sigma
+    ID = Base.TestID.BLOCKFREQUENCY
+    NAME = "Frequency Test within a Block"
 
-    p_value = gammaincc(blk_num/2 , chi_square/2)
+    def __init__(self, seq_len, seq_num, blk_len=128, proc_num=1,
+                init=True) -> None:
+        """
+        Parameters
+        ----------
+        seq_len : int
+            Bit length of each sequence.
+
+        seq_num : int
+            Number of sequences.
+            If more than 1, the loaded sequence is split and tested separately.
+
+        blk_size : int
+            Bit length of each block.
+        """
+        if seq_len < blk_len:
+            msg = "The sequence length must be larger than the block length."
+            raise InvalidSettingError(msg)
+        if init:
+            super().__init__(seq_len, seq_num, proc_num)
+            # self.__tests.append(self)
+        self.__blk_len = blk_len
+        self.__blk_num = seq_len // blk_len
+    
+    def info(self) -> str:
+        """
+        Test information.
+
+        Returns
+        -------
+        msg : str
+            A string containing test parameters and results annotations.
+        """
+        msg = BlockFrequencyTest.ID.value + "\n"
+        msg += "Block length,{}\n".format(self.__blk_len)
+        msg += "Number of blocks,{}\n".format(self.__blk_num)
+        msg += "Partial results\n"
+        msg += "SequenceID,p-value,chi_square\n"
+        return msg
+    
+    def func(self, bits):
+        """
+        Main function.
+
+        Parameters
+        ----------
+        bits : 1d-ndarray uint8
+            Binary sequence to be tested.
         
-    return p_value, chi_square
-
-
-if __name__ == "__main__":
-
-    bits = np.random.randint(0, 2, size=10**6)
-    results = frequency(bits)
-    print("\np-value = {}\n".format(results[0]))
+        Returns
+        -------
+        p_value : float
+            Test result.
+        chi_square : float
+            Computed statistic.
+        """
+        blk = np.resize(bits, (self.__blk_num, self.__blk_len))
+        sigma = np.sum((np.sum(blk, axis=1)/self.__blk_len - 0.5)**2)
+        chi_square = 4 * self.__blk_len * sigma
+        p_value = gammaincc(self.__blk_num/2 , chi_square/2)
+        return p_value, chi_square
--- tests/monobit.py
+++ tests/monobit.py
@@ -1,37 +1,74 @@
 # monobit.py
 
+from tests.base import Base
 import numpy as np
 from math import sqrt, erfc
 
 
-def monobit(bits):
+class FrequencyTest(Base):
     """
     Frequency (Monobit) Test
-    ------------------------
+    ========================
     Evaluate the uniformity of 0s and 1s for the entire sequence.
 
-    Parameters
+    Attributes
     ----------
-    bits : 1d-ndarray uint8
-        Binary sequence to be tested.
-    
-    Returns
-    -------
-    p_value : float
-        Test result.
-    counts : list of int
-        Number of occurrences of 0s and 1s for the entire sequence.
+
     """
-    ones = np.count_nonzero(bits)
-    zeros = bits.size - ones
+    ID = Base.TestID.FREQUENCY
+    NAME = "Frequency (Monobit) Test"
 
-    p_value = erfc((abs(ones-zeros)/sqrt(bits.size))/sqrt(2))
+    def __init__(self, seq_len, seq_num, proc_num=1, init=True) -> None:
+        """
+        Parameters
+        ----------
+        seq_len : int
+            Bit length of each sequence.
 
-    return p_value, [zeros, ones]
+        seq_num : int
+            Number of sequences.
+            If more than 1, the loaded sequence is split and tested separately.
+        
+        proc_num : int
+            Number of processes for running tests in parallel.
+        
+        init : bool
+            Whether to initialize super class.
+            To avoid redefinition of super class in multiple tests.
+        """
+        if init:
+            super().__init__(seq_len, seq_num, proc_num)
 
+    def func(self, bits):
+        """
+        Main function.
 
-if __name__ == "__main__":
+        Parameters
+        ----------
+        bits : 1d-ndarray uint8
+            Binary sequence to be tested.
+        
+        Returns
+        -------
+        p_value : float
+            Test result.
+        
+        counts : list of int
+            Number of occurrences of 0s and 1s for the entire sequence.
+        """
+        ones = np.count_nonzero(bits)
+        zeros = bits.size - ones
+        p_value = erfc((abs(ones-zeros)/sqrt(bits.size))/sqrt(2))
+        return p_value, zeros, ones
 
-    bits = np.random.randint(0, 2, size=10**6)
-    results = monobit(bits)
-    print("\np-value = {}\n".format(results[0]))
+    def report(self, results: list) -> str:
+        """
+        Generate a CSV string from the partial test results.
+        Use save_report() to generate a CSV for the entire test.
+        """
+        msg = FrequencyTest.NAME + "\n"
+        msg += "Partial results\n"
+        msg += "SequenceID,p-value,zeros,ones\n"
+        for i, j in enumerate(results):
+            msg += "{},{},{},{}\n".format(i, j[0], j[1], j[2])
+        return msg
